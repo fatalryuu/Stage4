d80b784fc833c51dd1fc3d2559a33b841411b24f
fatalryuu <vanyax941@gmail.com>
2023-05-30 14:16:21 +0300

    feat: mobile version

--- src/Components/Projects/Project/Project.jsx
+++ src/Components/Projects/Project/Project.jsx
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 
 const Project = ({info}) => {
     return (
-        <article>
-            <a href="/">
+        <article className={s.project__wrapper}>
+            <a href="/" className={s.project__link}>
                 <img src={info.imgURL} alt="" className={s.project__icon}></img>
                 <div className={s.project__text}>
                     <h2 className={s.project__header}>{info.title}</h2>
