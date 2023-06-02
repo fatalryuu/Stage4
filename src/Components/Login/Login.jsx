4ff31bd9c4e28619095ce961ccc45cdd4137a5e2
DrEiZ3R <vanyax941@gmail.com>
2023-06-02 16:11:34 +0300

    feat: add login logic

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 import s from './Login.module.css';
-import {NavLink} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+// import {addUser} from "../../Redux/slices/users";
 
 const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+    const users = useSelector(state => state.users.users);
+    // const dispatch = useDispatch();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (users.find(user => user.username === username && user.password === password)) {
+            navigate("/");
+        }
+    }
+
     return (
         <div className={s.wrapper}>
-            <input type="text"/>
-            <input type="text"/>
-            <NavLink to="/">Login</NavLink>
+            {/*<button onClick={() => dispatch(addUser({username: "root", password: "root"}))}>Add</button>*/}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    className={s.username}
+                    required={true}
+                />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    className={s.password}
+                    required={true}
+                />
+                <input type="submit" value="Login"/>
+            </form>
         </div>
     );
 };
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import navbarSlice from "./slices/navbar";
 import projectsSlice from "./slices/projects";
+import usersSlice from "./slices/users";
 
 const store = configureStore({
     reducer: {
         navbar: navbarSlice,
-        projects: projectsSlice
+        projects: projectsSlice,
+        users: usersSlice
     }
 });
 
