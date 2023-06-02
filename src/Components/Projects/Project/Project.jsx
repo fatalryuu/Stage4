633f84165e395ab4237fe1f043aa29066c3b2017
DrEiZ3R <vanyax941@gmail.com>
2023-06-02 15:33:00 +0300

    fix: projects  a -> NavLink

--- src/Components/Projects/Project/Project.jsx
+++ src/Components/Projects/Project/Project.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import s from './Project.module.css';
 import PropTypes from "prop-types";
+import {NavLink} from "react-router-dom";
 
 const Project = ({info}) => {
     return (
         <article className={s.project__wrapper}>
-            <a href="/" className={s.project__link}>
+            <NavLink to="/" className={s.project__link}>
                 <img src={info.imgURL} alt="" className={s.project__icon}></img>
                 <div className={s.project__text}>
                     <h2 className={s.project__header}>{info.title}</h2>
                     <p className={s.project__description}>{info.text}</p>
                 </div>
-            </a>
+            </NavLink>
         </article>
     );
 };
