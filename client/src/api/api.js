d66259a9e40d7355dfc43aa61f32359cb03b8066
DrEiZ3R <vanyax941@gmail.com>
2023-06-07 22:43:53 +0300

    feat: create tests for login

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -2,17 +2,16 @@ import axios from "axios";
 import { login as loginAC } from "../redux/slices/auth";
 import { addProjects } from "../redux/slices/projects";
 
-const instance = axios.create({
-    baseURL: "http://localhost:5000/api",
-});
-
 export const login = (username, password) => {
     return async dispatch => {
         try {
-            const response = await instance.post("auth/login", {
-                username,
-                password,
-            });
+            const response = await axios.post(
+                "http://localhost:5000/api/auth/login",
+                {
+                    username,
+                    password,
+                },
+            );
             dispatch(loginAC({ id: response.data.id }));
 
             return response.data;
@@ -28,7 +27,9 @@ export const login = (username, password) => {
 export const getProjects = () => {
     return async dispatch => {
         try {
-            const response = await instance.get("data/projects");
+            const response = await axios.get(
+                "http://localhost:5000/api/data/projects",
+            );
 
             dispatch(addProjects({ projects: response.data }));
             return response.data;
--- client/src/tests/login.test.js
+++ client/src/tests/login.test.js
@@ -1,1 +1,43 @@
-describe("login", () => {});
+import axios from "axios";
+import { login as loginAC } from "../redux/slices/auth";
+import { login } from "../api/api";
+
+jest.mock("axios");
+
+describe("login", () => {
+    it("successful login", async () => {
+        const responseId = 1;
+        const mockDispatch = jest.fn();
+        axios.post.mockResolvedValue({ data: { id: responseId } });
+
+        await login("admin", "1234")(mockDispatch);
+
+        expect(axios.post).toHaveBeenCalledWith(
+            "http://localhost:5000/api/auth/login",
+            {
+                username: "admin",
+                password: "1234",
+            },
+        );
+        expect(mockDispatch).toHaveBeenCalledWith(loginAC({ id: responseId }));
+    });
+
+    it("login failed", async () => {
+        const responseMessage = "Invalid username or password";
+        const mockDispatch = jest.fn();
+        axios.post.mockRejectedValue({
+            response: { data: { message: responseMessage } },
+        });
+
+        await login("admin1", "1234")(mockDispatch);
+
+        expect(axios.post).toHaveBeenCalledWith(
+            "http://localhost:5000/api/auth/login",
+            {
+                username: "admin1",
+                password: "1234",
+            },
+        );
+        expect(mockDispatch).not.toHaveBeenCalledWith(loginAC());
+    });
+});
